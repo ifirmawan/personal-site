5c3e7a0803a0d80f145cc6c087bb1a1cc5928fa3
Iwan Firmawan <firmawaneiwan@gmail.com>
2020-11-13 21:48:34 +0700

    add favicon image

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html>
 <head>
 	 <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<title>Iwan Firmawan</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon-96x96.png"/>
 </head>
 <body>
 <p>Hi</p>
