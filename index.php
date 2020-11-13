dbe4c9cf537a7b95aff92e0f3520facbf32ad9c8
Iwan Firmawan <firmawaneiwan@gmail.com>
2020-11-13 21:53:42 +0700

    change format favicon

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<title>Iwan Firmawan</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon-96x96.png"/>
+    <link rel="icon" href="images/favicon-96x96.png" sizes="96x96" type="image/png" />
 </head>
 <body>
 <p>Hi</p>
